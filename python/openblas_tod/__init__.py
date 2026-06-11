b1a20580160086de405b6bb8d653748d578ee2f6
Itamar Turner-Trauring <itamar@itamarst.org>
2026-06-11 13:53:52 -0400

    Sketch of custom threads callback.

---
+++ python/openblas_tod/__init__.py
@@ -0,0 +1,23 @@
+"""
+Install a custom threading model into OpenBLAS.
+"""
+
+import threadpoolctl
+
+from ._openblas_tod import _install
+
+
+def install() -> None:
+    """Install the custom threading model."""
+    # TODO single-threaded for now
+    threadpoolctl.threadpool_limits(limits=1, user_api="blas")
+    for library in threadpoolctl.threadpool_info():
+        if (
+            library["user_api"] == "blas"
+            and library["internal_api"] == "openblas"
+            and library["threading_layer"] == "pthreads"
+        ):
+            _install(library["filepath"])
+
+
+__all__ = ["install"]
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,82 @@
+use libloading::{Library, Symbol};
+use std::{ffi::c_int, os::raw::c_void, sync::OnceLock};
+
 use pyo3::prelude::*;
 
-/// A Python module implemented in Rust.
+#[allow(non_camel_case_types)]
+pub type openblas_dojob_callback =
+    Option<extern "C" fn(thread_num: c_int, jobdata: *mut c_void, dojob_data: c_int)>;
+
+#[allow(non_camel_case_types)]
+pub type openblas_threads_callback = Option<
+    extern "C" fn(
+        sync: c_int,
+        dojob: openblas_dojob_callback,
+        numjobs: c_int,
+        jobdata_elsize: usize,
+        jobdata: *mut c_void,
+        dojob_data: c_int,
+    ),
+>;
+
+/// The loaded OpenBLAS library, once we have it:
+static OPENBLAS: OnceLock<Library> = OnceLock::new();
+
+/// API to get number of threads:
+#[allow(non_upper_case_globals)]
+static openblas_get_num_threads: OnceLock<Symbol<unsafe extern "C" fn() -> c_int>> =
+    OnceLock::new();
+
+fn install(dll_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let library = unsafe { Library::new(dll_path)? };
+    OPENBLAS.set(library);
+    openblas_get_num_threads.set(
+        OPENBLAS
+            .get()
+            .unwrap()
+            .get(b"openblas_get_num_threads")
+            .unwrap()?,
+    );
+    let install_threads_callback: Symbol<
+        unsafe extern "C" fn(callback: openblas_threads_callback),
+    > = OPENBLAS
+        .get()
+        .unwrap()
+        .get(b"openblas_set_threads_callback_function")
+        .unwrap(); // TODO older versions won't have this API...
+    install_threads_callback(Some(run_in_threads_callback));
+    Ok(())
+}
+
+extern "C" fn run_in_threads_callback(
+    sync: c_int,
+    dojob: openblas_dojob_callback,
+    numjobs: c_int,
+    jobdata_elsize: usize,
+    jobdata: *mut c_void,
+    dojob_data: c_int,
+) {
+    let numjobs = numjobs as isize;
+    let jobdata_elsize = jobdata_elsize as isize;
+    // TODO no thread pool yet
+    for i in 0..numjobs {
+        let element_addr = unsafe { jobdata.byte_offset(i * jobdata_elsize) };
+        dojob.unwrap()(i as c_int, element_addr, dojob_data);
+    }
+}
+
+/// Switch out OpenBLAS pthreads global thread pool module with an on-demand
+/// thread pool, per thread. This matches the behavior of OpenBLAS compiled with
+/// OpenMP.
 #[pymodule]
 mod openblas_tod {
+    use super::install;
     use pyo3::prelude::*;
 
-    /// Formats the sum of two numbers as string.
+    /// Install a new thread pool model into the given OpenBLAS shared library.
     #[pyfunction]
-    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-        Ok((a + b).to_string())
+    fn _install(dll_path: &str) -> PyResult<()> {
+        install(dll_path).unwrap();
+        Ok(())
     }
 }
